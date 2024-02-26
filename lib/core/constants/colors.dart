a3a8bfbbe6270f8b8bb5cbfdf2b898abb15ac9a1
najeebnaju512 <najeebnaju512@gmail.com>
2024-02-26 13:34:57 +0530

    bottoom navigation added, main changed

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static Color yellow = Color(0xFFF5C754);
+  static Color white = Color(0xFFffffff);
+  static Color black = Color.fromARGB(255, 0, 0, 0);
+}
---
+++ lib/presentation/bottom_navigation/view/bottom_navigation.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../../../core/constants/colors.dart';
+import '../controller/bottom_nav_controller.dart';
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        body: Consumer<BottomNavigationController>(
+          builder: (context, provider, child) {
+            return IndexedStack(
+              index: provider.currentIndex,
+              children: [],
+            );
+          },
+        ),
+        bottomNavigationBar: Consumer<BottomNavigationController>(
+          builder: (context, provider, child) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 5,right: 5,bottom: 5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: BottomNavigationBar(
+                  onTap: (index) {
+                    provider.currentIndex = index;
+                    print("tapped ${provider.currentIndex}");
+                  },
+                  
+                  type: BottomNavigationBarType.fixed,
+                  currentIndex: provider.currentIndex,
+                  elevation: 0,
+                 backgroundColor: ColorTheme.white,
+                  unselectedItemColor: ColorTheme.black,
+                  selectedItemColor: ColorTheme.yellow,
+                  showSelectedLabels: true,
+                  showUnselectedLabels: false,
+                  selectedLabelStyle: GoogleFonts.kanit(fontSize: 11),
+                  items: [
+                    BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.home,
+                        ),
+                        label: "Home",
+                        activeIcon: Icon(Icons.home_filled)),
+                    BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.payments_outlined,
+                        ),
+                        label: "Transaction",
+                        activeIcon: Icon(Icons.payments)),
+                    BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.payment_outlined,
+                        ),
+                        label: "Payments",
+                        activeIcon: Icon(Icons.payment)),
+                    BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.person_2_outlined,
+                        ),
+                        label: "Profile",
+                        activeIcon: Icon(Icons.person_2)),
+                        
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
