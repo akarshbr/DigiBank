645b3666e0a1f2136608c536a508ca1b2d977e5c
najeebnaju512 <najeebnaju512@gmail.com>
2024-02-27 17:28:22 +0530

    updated home card/requestmoney page

---
+++ lib/presentation/request_money/view/request_money.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RequestMoney extends StatelessWidget {
+  const RequestMoney({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
