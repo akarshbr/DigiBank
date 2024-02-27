a249a96926576734c1d155b3b764e608acdda0c9
akarshbr <akarshbrajeev@gmail.com>
2024-02-27 12:02:21 +0530

    text,pin and otp,text form field added

---
+++ lib/global_widget/pin_and_otp_box.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PinAndOtpBox extends StatelessWidget {
+  const PinAndOtpBox({super.key, this.gap = 1});
+
+  final double gap;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      height: 64,
+      width: 64,
+      margin: EdgeInsets.only(left: gap),
+      decoration: BoxDecoration(border: Border.all(), borderRadius: BorderRadius.circular(10)),
+      child: TextFormField(
+        onChanged: (value) {
+          if (value.length == 1) {
+            FocusScope.of(context).nextFocus();
+          }
+        },
+        decoration: InputDecoration(
+          border: InputBorder.none
+        ),
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly
+        ],
+      ),
+    );
+  }
+}
