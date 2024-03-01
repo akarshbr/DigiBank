3832523a561723ba962a2f424314cab574ea4c0b
akarshbr <akarshbrajeev@gmail.com>
2024-03-01 15:55:42 +0530

    electricity bill screen modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:digibank/presentation/account_summary_screen/controller/account_summary_control.dart';
 import 'package:digibank/presentation/home_screen/controller/home_controler.dart';
 import 'package:digibank/presentation/profile_screen/controller/profile_controller.dart';
+import 'package:digibank/presentation/recharge_and_pay_bills/electricity/controller/electricity_controller.dart';
 import 'package:digibank/presentation/recharge_and_pay_bills/mobile_recharge/controller/operator_controller.dart';
 import 'package:digibank/presentation/registration_screen/view/registration.dart';
 import 'package:digibank/presentation/transaction_screen/controller/transaction_controller.dart';
@@ -11,8 +12,7 @@ import 'package:provider/provider.dart';
 import 'presentation/bottom_navigation_screen/controller/bottom_nav_controller.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.grey));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.grey));
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (context) => BottomNavigationController()),
     ChangeNotifierProvider(create: (context) => HomeController()),
@@ -20,6 +20,7 @@ void main() {
     ChangeNotifierProvider(create: (context) => OperatorController()),
     ChangeNotifierProvider(create: (context) => TransactionController()),
     ChangeNotifierProvider(create: (context) => ACCSumaryCantrol()),
+    ChangeNotifierProvider(create: (context) => ElectricityController())
     // ChangeNotifierProvider(
     //   create: (context) =>
     // ),
--- lib/presentation/recharge_and_pay_bills/electricity/view/electricity_bills_screen.dart
+++ lib/presentation/recharge_and_pay_bills/electricity/view/electricity_bills_screen.dart
@@ -1,13 +1,19 @@
+import 'package:digibank/global_widget/title_and_textformfield.dart';
+import 'package:digibank/presentation/recharge_and_pay_bills/electricity/controller/electricity_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../core/constants/colors.dart';
 import '../../../../core/constants/global_text_style.dart';
 import '../../../../global_widget/global_appbar.dart';
+import '../../../../global_widget/text_refactor.dart';
 
 class ElectricityBillsScreen extends StatelessWidget {
   const ElectricityBillsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: GLAppBar(
         title: "Electricity",
@@ -19,7 +25,65 @@ class ElectricityBillsScreen extends StatelessWidget {
             },
             icon: Icon(Icons.arrow_back)),
       ),
-      body: Center(child: Text("Electricity")),
+      body: Center(
+        child: Container(
+          height: size.height * .5,
+          width: size.width * 0.9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Consumer<ElectricityController>(builder: (builder, electricityController, _) {
+                return DropdownButton(
+                    padding: EdgeInsets.all(5),
+                    isExpanded: true,
+                    items: dropDownItems,
+                    value: electricityController.providerSelected,
+                    onChanged: (String? selectedProvider) {
+                      electricityController.setProvider(selectedProvider!);
+                    });
+              }),
+              TitleAndTextFormField(text: "Enter Consumer Number"),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child:
+                      TextRefactor(text: "PROCEED", textSize: 16, textFontWeight: FontWeight.bold),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorTheme.mainClr,
+                      padding: EdgeInsets.only(
+                          left: size.width * .2,
+                          right: size.width * .2,
+                          top: size.height * .02,
+                          bottom: size.height * .02),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+List<DropdownMenuItem<String>> get dropDownItems {
+  List<DropdownMenuItem<String>> menuItems = [
+    DropdownMenuItem(
+        child: ListTile(
+            leading: Image(width: 30, image: AssetImage("asset/electricity_icon/KSEB_Logo.png")),
+            title: Text("KSEB")),
+        value: "KSEB"),
+    DropdownMenuItem(
+        child: ListTile(
+            leading:
+                Image(width: 30, image: AssetImage("asset/electricity_icon/Tata_Power_Logo.png")),
+            title: Text("TATA Power")),
+        value: "TATA Power"),
+    DropdownMenuItem(
+        child: ListTile(
+            leading: Image(width: 30, image: AssetImage("asset/electricity_icon/adaniPower.png")),
+            title: Text("ADANI Power")),
+        value: "ADANI Power"),
+  ];
+  return menuItems;
 }
