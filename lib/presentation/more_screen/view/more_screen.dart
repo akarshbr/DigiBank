311075af333dc891abea4f22ac6831e74e22dccf
akarshbr <akarshbrajeev@gmail.com>
2024-03-01 10:25:17 +0530

    more screen modified

--- lib/presentation/more_screen/view/more_screen.dart
+++ lib/presentation/more_screen/view/more_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/constants/global_text_style.dart';
+import '../../../global_widget/global_appbar.dart';
+
 class MoreScreen extends StatelessWidget {
   const MoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: GLAppBar(
+        title: "More",
+        centerTitle: true,
+        titleTextStyle: GLTextStyles.titleStyle,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back)),
+      ),
       body: Center(child: Text("Under Construction")),
     );
   }
