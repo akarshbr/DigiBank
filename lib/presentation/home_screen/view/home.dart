3dda88fa2959aab9a1417a45c6fed2ca2a048c5f
NITHA <nithaparveenk@gmail.com>
2024-03-05 12:55:53 +0530

    title textstyle changed

--- lib/presentation/home_screen/view/home.dart
+++ lib/presentation/home_screen/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:digibank/core/constants/colors.dart';
 import 'package:digibank/core/constants/global_text_style.dart';
 import 'package:digibank/global_widget/global_appbar.dart';
 import 'package:digibank/presentation/home_screen/view/widget/advertisment_slider.dart';
@@ -6,9 +7,9 @@ import 'package:digibank/presentation/home_screen/view/widget/tools_to_use.dart'
 import 'package:digibank/presentation/home_screen/view/widget/user_details.dart';
 import 'package:digibank/presentation/home_screen/view/widget/user_function.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../global_widget/drawer_refactored.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,16 +19,25 @@ class HomeScreen extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: GLAppBar(
-          title: 'DigiBank',
+        appBar: AppBar(
+          title: Text('digiBank',
+              style: GoogleFonts.leagueSpartan(
+                color: ColorTheme.darkClr,
+                fontSize: 23.0,
+                fontWeight: FontWeight.w600,
+              )),
           centerTitle: false,
           actions: [
-            IconButton(onPressed: () {
-              // TODO
-            }, icon: const Icon(Icons.notifications)),
-            IconButton(onPressed: () {
-              // TODO
-            }, icon: const Icon(Icons.logout_outlined)),
+            IconButton(
+                onPressed: () {
+                  // TODO
+                },
+                icon: const Icon(Icons.notifications)),
+            IconButton(
+                onPressed: () {
+                  // TODO
+                },
+                icon: const Icon(Icons.logout_outlined)),
             const SizedBox(
               width: 10,
             )
@@ -54,12 +64,12 @@ class HomeWidget extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
       child: ListView(
         children: [
-          //users functions card  
+          //users functions card
           UsersFunctions(size, context),
           SizedBox(
             height: size.height * .05,
           ),
-          //user details 
+          //user details
           UserDetailsCard(size),
           SizedBox(
             height: size.height * .05,
