34606b088efd1ed18a5f84f0a2d305f5618dffba
akarshbr <akarshbrajeev@gmail.com>
2024-03-11 11:08:36 +0530

    sharedpreference used to store username user entered in registration

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -4,4 +4,6 @@ class AppConfig {
 
   //key to store data from api in json format used in AppUtils to get accesskey&userid
   static const String LOGIN_DATA = 'logInData';
+  //key to store username which user entered used in registration page controller, login page controller
+  static const String userName="username";
 }
--- lib/presentation/registration_screen/controller/registration_controller.dart
+++ lib/presentation/registration_screen/controller/registration_controller.dart
@@ -1,23 +1,31 @@
 import 'dart:developer';
 
+import 'package:digibank/app_config/app_config.dart';
 import 'package:digibank/core/app_utils/app_utils.dart';
 import 'package:digibank/presentation/login_screen/view/login_screen.dart';
 import 'package:digibank/repository/api/registration_screen/services/registration_service.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegistrationController extends ChangeNotifier {
-  Future onRegistration(
-      String username, String accountNumber, var context) async {
-    var data = {"username": username, "account_number": accountNumber};
+  late SharedPreferences sharedPreferences;
+
+  Future onRegistration(String enteredUsername, String accountNumber, var context) async {
+    var data = {"username": enteredUsername, "account_number": accountNumber};
     RegistrationService.postRegistratioonData(data).then((resData) {
       log("onRegistration ${resData["status"]}");
       if (resData["status"] == 1) {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const LoginScreen()));
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+        storeUserName(enteredUsername);
       } else {
         var message = resData["error"];
         AppUtils.oneTimeSnackBar(message, context: context);
       }
     });
+  }
+
+  void storeUserName(String enteredUsername) async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString(AppConfig.userName, enteredUsername);
   }
 }
