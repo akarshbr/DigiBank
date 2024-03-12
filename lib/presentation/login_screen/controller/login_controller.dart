1ce79e846435c6b3b720bb2fe6fe01eec8b1ec78
najeebnaju512 <najeebnaju512@gmail.com>
2024-03-12 11:03:15 +0530

    added get method

--- lib/presentation/login_screen/controller/login_controller.dart
+++ lib/presentation/login_screen/controller/login_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:digibank/app_config/app_config.dart';
-import 'package:digibank/presentation/home_screen/view/home.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
--- lib/repository/helper/api_helper.dart
+++ lib/repository/helper/api_helper.dart
@@ -10,10 +10,8 @@ class ApiHelper {
   static getData({
     required String endPoint,
     Map<String, String>? header,
-    required Map<String, dynamic> body,
   }) async {
     log("ApiHelper>getData");
-    log("$body");
     var url = Uri.parse(AppConfig.baseurl + endPoint);
     try {
       var response = await http.get(url);
@@ -21,13 +19,13 @@ class ApiHelper {
       if (response.statusCode == 200) {
         var data = response.body;
         var decodedData = jsonDecode(data);
-        // log(decodedData["status"].toString());
+        log(decodedData["status"].toString());
         return decodedData;
       } else {
         log("Else Condition >> Api failed");
         var data = response.body;
         var decodedData = jsonDecode(data);
-        // log(decodedData["status"].toString());
+        log(decodedData["status"].toString());
         return decodedData;
       }
     } catch (e) {
@@ -44,7 +42,6 @@ class ApiHelper {
     log("Apihelper>postData");
     log("$body");
     var url = Uri.parse(AppConfig.baseurl + endPoint);
-    // var url = Uri.parse("http://10.11.0.186:8000/");
     try {
       var response = await http.post(url, body: body);
       log("Api Called => status code=${response.statusCode}");
